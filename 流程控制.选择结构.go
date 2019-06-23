5f5ca0c0e1774d395e8e48e2b02a93224f895bbd
liuwanyong2017 <2674706698@qq.com>
2019-06-23 17:32:47 +0800

    if else 的使用，else后面不能跟着条件，if可以。else的好处就是判断的执行就是一次，如果不用else，而是多个if，就要多次判断

--- 流程控制.选择结构.go
+++ 流程控制.选择结构.go
@@ -8,8 +8,14 @@ func main() {
 	fmt.Scan(&score)
 	if score > 680 {
 		fmt.Println("恭喜！清华北大在等你哦！！流弊！")
+	} else if score < 200 {
+		fmt.Printf("%d太低了！！蓝翔可以吗？\n", score)
+	} else if score > 200 && score < 500 {
+		fmt.Printf("%d还不错！可以去专科和二本啦！", score)
+	} else if score > 500 && score < 600 {
+		fmt.Printf("%d不错！可以去一本啦！", score)
 	} else {
-		fmt.Printf("%d太低了！！垃圾！\n", score)
+		fmt.Printf("%dliubi！可以去211啦！", score)
 	}
 	//初始化语句，需要分号去分割开条件
 	if a := 650; a > 700 {
