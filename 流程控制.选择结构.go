0f284f4a372a91b7dfe60739d2e62e03393e8427
liuwanyong2017 <2674706698@qq.com>
2019-06-24 01:07:56 +0800

    switch,throughAll 与 break ，默认值， 多个值放一起判断，区间判断

--- 流程控制.选择结构.go
+++ 流程控制.选择结构.go
@@ -32,4 +32,25 @@ func main() {
 	} else {
 		fmt.Printf("%d太低了！！垃圾！\n", a)
 	}
+	switch { //不建议嵌套，适合离散型的
+	case score > 740:
+		fmt.Printf("%d可以去学厨师啦！\n", score)
+	case score > 720:
+		fmt.Printf("%d可以去学挖掘机啦！\n", score)
+	case score > 700:
+		fmt.Printf("%d可以去学洗剪吹啦！\n", score)
+		fallthrough //匹配后，继续匹配下面的，默认为break形式
+	case score > 680, score > 650, score > 640:
+		fmt.Printf("%d可以去985啦！\n", score)
+	default:
+		fmt.Printf("%d你流弊啊！", score)
+	}
+	switch s1 := 650; score { //如果score没有值，就用默认值
+	case 650:
+		fmt.Printf("%d默认值测试！", s1)
+	case 640, 630, 620:
+		fmt.Printf("%d玄学啊！", s1)
+	default:
+		fmt.Println("不知道你考得啥意思！")
+	}
 }
