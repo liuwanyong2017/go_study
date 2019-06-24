084a1613e0d9ce490978dcc8697829ac2642946d
liuwanyong2017 <2674706698@qq.com>
2019-06-24 10:41:50 +0800

    for 嵌套使用，尽量能不嵌套就不嵌套，百钱买百鸡的问题

--- 循环语句.go
+++ 循环语句.go
@@ -5,8 +5,10 @@ import "fmt"
 func main() {
 	//初始化条件，判断条件，变化条件
 	//i 块级变量
+	sum := 0
 	for i := 1; i < 10; i++ {
-		fmt.Println(i)
+		sum = sum + i
+		fmt.Println(i, sum)
 	}
 	str := "liuliuwan"
 	for i := 0; i < len(str); i++ {
@@ -24,4 +26,33 @@ func main() {
 		fmt.Printf("%d\n", data)
 		fmt.Printf("%c\n", data)
 	}
+	//嵌套
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 5; j++ {
+			fmt.Println(i, j)
+		}
+	}
+	//百鸡问题
+	count := 0
+	count1 := 0
+	for man := 0; man <= 20; man++ {
+		for woman := 0; woman <= 33; woman++ {
+			child := 100 - man - woman
+			count++
+			if child%3 == 0 && (man*5+woman*3+child/3 == 100) {
+				fmt.Printf("man:%d,woman:%d,child:%d\n", man, woman, child)
+			}
+		}
+	}
+	for man := 0; man <= 20; man++ {
+		for woman := 0; woman <= 33; woman++ {
+			for child := 0; child <= 100; child += 3 {
+				count1++
+				if child+woman+man == 100 && (child/3+woman*3+man*5 == 100) {
+					fmt.Printf("man:%d,woman:%d,child:%d\n", man, woman, child)
+				}
+			}
+		}
+	}
+	fmt.Printf("count=%d,count1=%d", count, count1)
 }
