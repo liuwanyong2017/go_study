2bfc9f014ac293fb2c497e04511d1db6077fbe03
liuwanyong2017 <2674706698@qq.com>
2019-06-25 00:22:28 +0800

    函数的类名更改，变量赋值为一个函数

--- 函数.go
+++ 函数.go
@@ -10,9 +10,7 @@ func sum(a int, arg ...int) {
 	}
 	fmt.Println(res, a)
 }
-func s(a, b int) int {
-	return a + b
-}
+
 func t1(args ...int) {
 	t2(args[0:2]...)
 	t3(args[2:4]...) //取一段的参数
@@ -28,8 +26,27 @@ func t3(args ...int) {
 func t4(args ...int) {
 	fmt.Println("4", args)
 }
+
+type Sum func(int, int) int //自定义函数类型
+func s(a, b int) int {
+	return a + b
+} //返回值 1个的时候
+func s1(a, b int) (c, d, e int) {
+	e = a + b
+	c = a
+	d = b
+	return
+} //返回值多个的时候
 func main() {
 	sum(12, 3, 34, 3, 4545, 4)
 	fmt.Println(s(2, 5))
 	t1(3, 23, 8, 3, 23, 55, 43, 4)
+	x, y, z := s1(3, 4)
+	fmt.Println(x, y, z)
+	_, r, _ := s1(5, 6) //返回多个值的时候，默认变量的好处
+	fmt.Println(r)
+
+	var k Sum
+	k = s //函数的自定义
+	fmt.Println(k(4, 5))
 }
